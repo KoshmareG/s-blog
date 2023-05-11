59526f42016cff201d62e8ee9877438e8e9899c0
KoshmareG <koshmareg@yandex.ru>
2023-05-11 19:58:40 +0700

    refactor: removed user creation

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { create(:user) }
-
   context 'validations' do
     it { should validate_presence_of(:username) }
     it { should validate_length_of(:username).is_at_most(36) }
