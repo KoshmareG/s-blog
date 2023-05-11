211dc72fcb8d6a0c325591d934050d5c943e6bf3
KoshmareG <koshmareg@yandex.ru>
2023-05-11 16:35:17 +0700

    feat: add edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
+  before_action :set_post, except: %i[index new create]
 
   def index
     @posts = Post.includes(picture_attachment: :blob).all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -23,9 +23,24 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: I18n.t('controllers.posts.updated')
+    else
+      render 'posts/edit', status: :unprocessable_entity
+    end
+  end
+
   private
 
   def post_params
     params.require(:post).permit(:title, :body, :picture)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
