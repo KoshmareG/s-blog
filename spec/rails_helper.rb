a284311faa11f6922f90d9a0264ab80aacf97986
KoshmareG <koshmareg@yandex.ru>
2023-05-11 19:47:08 +0700

    feat: add shulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,3 +66,10 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include Warden::Test::Helpers, type: :feature
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
